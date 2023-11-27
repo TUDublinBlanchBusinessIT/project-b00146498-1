1297ee80fc01cc1d5b8280b93cb30c1a2b238330
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2023-11-27 14:16:07 +0000

    created folder called versions and added in V1 of database

---
+++ VersionsDB/bettingshopDBV1.sql.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE IF NOT EXISTS bettingshop;
+USE bettingshop;
+DROP TABLE IF EXISTS horsebet;
+CREATE TABLE horsebet (
+    id INTEGER AUTO_INCREMENT,
+    horsename VARCHAR(30),
+    place VARCHAR(30),
+    time TIME,
+    dateofrace DATE,
+    stake INT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO horsebet (horsename, place, time, dateofrace, stake)
+VALUES ('Ross', 'Navan', '15:00:00', '2023-10-01', 6);
+
+SELECT * FROM horsebet;
+
+ALTER TABLE horsebet
+MODIFY COLUMN stake DECIMAL(10,2);
+
+INSERT INTO horsebet (horsename, place, time, dateofrace, stake)
+VALUES ('Sweetie', 'Navan', '15:00:00', '2023-10-01', 25.20);
+
+SELECT * FROM horsebet
+
+CREATE TABLE dogbet (
+    id INTEGER AUTO_INCREMENT,
+    dogname VARCHAR(30),
+    trapno INT,
+    place VARCHAR(30),
+    time TIME,
+    dateofrace DATE,
+    stake INT,
+    PRIMARY KEY (id)
+);
+INSERT INTO dogbet (dogname, trapno, place, TIME, dateofrace, stake)
+VALUES ('Hello', 6, 'Curragh', '15:00:00', '2023-10-01', 6);
+
+ALTER TABLE dogbet
+MODIFY COLUMN stake DECIMAL(10,2);
+
+INSERT INTO dogbet (dogname, trapno, place, time, dateofrace, stake)
+VALUES ('Hello', 6, 'Curragh', '15:00:00', '2023-10-01', 6.75);
+
+SELECT * FROM dogbet;
