2082e1fd13fcfc671d4302a20938f36da31eb0ed
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2023-11-27 14:51:02 +0000

    inserted 2 dropdown menus in dogbet html and works in sql too

---
+++ VersionsDB/bettingshopDBV2.sql
@@ -0,0 +1,25 @@
+SELECT * FROM horsebet
+
+CREATE TABLE dogbet (
+    id INTEGER AUTO_INCREMENT,
+    dogname VARCHAR(30),
+    trapno INT,
+    place VARCHAR(30),
+    time TIME,
+    dateofrace DATE,
+    stake INT,
+    PRIMARY KEY (id)
+);
+INSERT INTO dogbet (dogname, trapno, place, TIME, dateofrace, stake)
+VALUES ('Hello', 6, 'Curragh', '15:00:00', '2023-10-01', 6);
+
+ALTER TABLE dogbet
+MODIFY COLUMN stake DECIMAL(10,2);
+
+INSERT INTO dogbet (dogname, trapno, place, time, dateofrace, stake)
+VALUES ('Hello', 6, 'Curragh', '15:00:00', '2023-10-01', 6.75);
+
+SELECT * FROM dogbet;
+
+ALTER TABLE dogbet
+MODIFY COLUMN trapno VARCHAR(10);
