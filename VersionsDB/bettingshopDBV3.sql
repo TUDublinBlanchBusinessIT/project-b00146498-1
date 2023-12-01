c3453659bbf686de7f19338aacfdc716061e8962
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2023-12-01 17:20:16 +0000

    new design customer html and working

--- VersionsDB/bettingshopDBV3.sql
+++ VersionsDB/bettingshopDBV3.sql
@@ -8,3 +8,20 @@ CREATE TABLE customer (
 
 INSERT INTO customer (firstname, surname) VALUES ('Ross', 'Smith');
 SELECT * FROM customer
+
+
+DROP TABLE IF EXISTS horsebet;
+CREATE TABLE horsebet (
+    id INTEGER AUTO_INCREMENT,
+    horsename VARCHAR(30),
+    place VARCHAR(30),
+    time TIME,
+    dateofrace DATE,
+    stake INT,
+    customerid int,
+    PRIMARY KEY (id),
+    foreign key(customerid) references customer(id)
+    
+);
+INSERT INTO horsebet (horsename, place, time, dateofrace, stake, customerid) VALUES ('Ross', 'Navan', '15:00:00', '2023-10-01', 6, 50);
+SELECT * FROM horsebet
